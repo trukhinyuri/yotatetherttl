7d066194cba59ab1b34bf1d054832ba45f8283f1
kolyanok <kolyan-okunok@ya.ru>
2015-03-06 14:20:40 +0300

    Создаем
    Это, в общем, первый коммит.

---
+++ app/src/main/java/ru/antiyotazapret/yotatetherttl/MainActivity.java
@@ -0,0 +1,69 @@
+package ru.antiyotazapret.yotatetherttl;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.CheckBox;
+import android.app.Activity;
+import ru.antiyotazapret.yotatetherttl.ShellExecuter;
+public class MainActivity extends Activity {
+    EditText input;
+    Button btn;
+    CheckBox cb0;
+    TextView out;
+    String command;
+    ShellExecuter exe = new ShellExecuter();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onMyClick(View v) {
+        command = "settings put global airplane_mode_on 1";
+        command += "\nam broadcast -a android.intent.action.AIRPLANE_MODE --ez state true";
+        command += "\nsettings put global tether_dun_required 0";
+        exe.Executer(command);
+        int ttlnumber = 63;
+        String error=null;
+        switch(v.getId())
+        {
+            case R.id.btn: ttlnumber=127; break;
+            case R.id.button2: ttlnumber=63; break;
+            case R.id.button3:    input = (EditText)findViewById(R.id.txt);
+                TextView tv = (TextView) findViewById(R.id.textView2);
+                if(input!=null) {
+                    input = (EditText)findViewById(R.id.txt);
+                    ttlnumber = Integer.parseInt(input.getText().toString());
+                    if (ttlnumber > 1 && ttlnumber < 255) {
+
+                    } else
+                    {
+                        error="Please, enter a correct TTL!";
+                    }
+                }
+                else
+                {
+                    error="Please, enter something.";
+                }
+                break;
+        }
+        TextView tv = (TextView) findViewById(R.id.textView2);
+        if(error==null) {
+            command = "echo \"" + ttlnumber + "\" > /proc/sys/net/ipv4/ip_default_ttl";
+            command += "\nsettings put global airplane_mode_on 0";
+            command += "\nam broadcast -a android.intent.action.AIRPLANE_MODE --ez state false";
+            exe.Executer(command);
+            tv.setText("OK. Now you can turn on tethering!");
+        }
+        else
+        {
+            tv.setText(error);
+        }
+    }
+
+
+ }
+
---
+++ app/src/main/java/ru/antiyotazapret/yotatetherttl/ShellExecuter.java
@@ -0,0 +1,24 @@
+package ru.antiyotazapret.yotatetherttl;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class ShellExecuter {
+    public ShellExecuter() {
+    }
+    public String Executer(String command) {
+        StringBuffer output = new StringBuffer();
+        Process p;
+        try {
+           p=Runtime.getRuntime().exec(new String[] {"su", "-c",command});
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = "";
+            while ((line = reader.readLine())!= null) {
+                output.append(line + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String response = output.toString();
+        return response;
+    }
+}
